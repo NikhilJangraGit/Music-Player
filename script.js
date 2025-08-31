dee06fc2a0183b57003c2919607213bc2d4e1900
Nikhil Jangra <nikhiljangra2202@gmail.com>
2025-08-31 10:08:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let play = document.getElementById("play");
+let pause = document.getElementById("pause");
+let audio = document.getElementById("audio");
+audio.onloadedmetadata = ()=>{
+    progress.max=audio.duration;
+    progress.value=audio.currentTime;
+}
+play.addEventListener("click",()=>{
+        audio.play();
+        play.style.display="none";
+        pause.style.display="block";
+});
+pause.addEventListener("click",()=>{
+    audio.pause();
+    play.style.display="block";
+    pause.style.display="none";
+});
+
+let progress= document.getElementById("progress");
+
+if (audio.play()) {
+        setInterval(()=>{
+            progress.value=audio.currentTime;
+        },500);
+        play.style.display="none";
+        pause.style.display="block";
+}
+progress.onchange=()=>{
+    audio.play();
+    audio.currentTime=progress.value;
+    play.style.display="none";
+    pause.style.display="block";
+}
