c95791b7ef5991c53a47e66c53f2e2b23cdde914
Nikhil Jangra <nikhiljangra2202@gmail.com>
2025-08-31 10:13:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ let audio = document.getElementById("audio");
 audio.onloadedmetadata = ()=>{
     progress.max=audio.duration;
     progress.value=audio.currentTime;
-    audio.play();
 }
 play.addEventListener("click",()=>{
         audio.play();
         play.style.display="none";
         pause.style.display="block";
 });
+if(pause.style.display=="block"){
+    audio.play();
+}  
 pause.addEventListener("click",()=>{
     audio.pause();
     play.style.display="block";
@@ -33,3 +35,4 @@ progress.onchange=()=>{
     pause.style.display="block";
 
 }
+
